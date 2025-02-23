e55dc7d575f7294fdd356d09a6923109a6674c45
spoke4492 <payeasy843@proton.me>
2025-02-23 18:45:05 +0100

    Primo commit - Creazione del progetto BabyCare-App

--- gradlew.bat
+++ gradlew.bat
@@ -1,13 +1,8 @@
 @echo off
 setlocal
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+REM Versione Locale
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
 
-set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+REM Versione Remota
+set DEFAULT_JVM_OPTS="-Xmx1024m -Xms256m"
